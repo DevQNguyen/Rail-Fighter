1176b8cc51a8bc912687a9e3d19232685695ff49
DevQNguyen <go.quang.nguyen@gmail.com>
2021-05-22 00:09:04 -0700

    L87, setup laser particle prefab for collision detection. L88, script CollisionHandler.cs disable player controls, wait x second, then reload current scene.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-    void OnCollisionEnter(Collision other)
-    {
-        Debug.Log($"{this.name} collided with: {other.gameObject.name}!");
-    }
+    [Tooltip("Float value for number of seconds to delay.")]
+    [SerializeField] float loadDelay = 1f;
 
     void OnTriggerEnter(Collider other)
     {
         Debug.Log($"{this.name} triggered: {other.name}!!");
+        RestartSequence();
+    }
+
+    void RestartSequence()
+    {
+        // Disable Player Controls script
+        GetComponent<PlayerControls>().enabled = false;
+        Debug.Log($"In [RestartSequence()] ");
+        // Wait 1 sec, invoke ReloadScene()
+        Invoke("ReloadScene", loadDelay);
+    }
+
+    void ReloadScene()
+    {
+        // Get current scene index
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        Debug.Log($"In [RestartScene()] sceneIndex: {sceneIndex}");
+        SceneManager.LoadScene(sceneIndex);
     }
 }
 
