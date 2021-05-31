40de340388634097d664cf911370d08c50fc5c4d
DevQNguyen <go.quang.nguyen@gmail.com>
2021-05-31 00:34:06 -0700

    L94-95, implement TextMeshPro UI for scoreboard

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,16 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
+    TMP_Text scoreText;
     int score;
+
+    private void Start()
+    {
+        // Get TMP_text component attached to this gameObject
+        scoreText = GetComponent<TMP_Text>();
+        if (scoreText == null)
+        {
+            Debug.Log("TextMeshPro Component Not Found.");
+        }
+        scoreText.text = "Start";
+    }
 
     public void AddScore(int enemyValue)
     {
-        Debug.Log($"[AddPoint()] Start Score: {score}");
         score += enemyValue;
-        Debug.Log($"Score: {score}");
+        scoreText.text = $"Score: {score}";
     }
 }
 
